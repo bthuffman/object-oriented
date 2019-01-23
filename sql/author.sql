2205a3b551b463706971767569213fe9df81b7a2
Brandon Huffman <bt_huffman@msn.com>
2019-01-23 13:23:12 -0700

    Added the author.sql file to github with the changes mentioned in the last commit and push (i.e. see the last commit and push).

---
+++ sql/author.sql
@@ -0,0 +1,20 @@
+-- Set collation of database to utf8
+ALTER DATABASE bhuffman1 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+--create the author entity
+CREATE TABLE author (
+   -- create table attributes
+	authorId BINARY(16) NOT NULL,
+	authorAvatarUrl VARCHAR(255),
+	authorActivationToken CHAR(32),
+	authorEmail VARCHAR(128) NOT NULL,
+	authorHash CHAR(97) NOT NULL,
+	authorUsername VARCHAR(32) NOT NULL,
+	-- this marks the following attributes unique
+	UNIQUE(authorEmail),
+	UNIQUE(authorUsername),
+	-- this creates an index
+	INDEX(authorEmail),
+   -- this creates the primary key
+	PRIMARY KEY(authorId)
+	);
